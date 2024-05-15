9ad695b12fa7815b9288262057d185755007d9a5
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-05-15 17:38:05 +0530

    Add logout functionality to user controller and routes

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -142,4 +142,24 @@ const loginUser = asyncHandler(async (req, res) => {
     );
 });
 
-export { register, loginUser };
+const logout = asyncHandler(async (req, res) => {
+  console.log(req.user);
+  const user = await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $unset: { refreshToken: 1 },
+    },
+    { new: true }
+  );
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged out successfully"));
+});
+
+export { register, loginUser, logout };
--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { register, loginUser } from "../controllers/user.controller.js"; // this method is used to import the register function from the user.controller.js file
+import { register, loginUser, logout } from "../controllers/user.controller.js"; // this method is used to import the register function from the user.controller.js file
 
 const router = Router(); // this method is used to create a new router object
 
@@ -9,6 +9,7 @@ router.route("/register").post(upload.single("profilePicture"), register); // th
 // calling multer function to upload single file and calling register function
 
 router.route("/login").post(loginUser);
+router.route("/logout").post(verifyJWT, logout);
 
 export default router; // this method is used to export the router object
 // Path: backend/src/routes/user.routes.js
