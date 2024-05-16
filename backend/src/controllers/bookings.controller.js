f46b443aa1620c2730cca35fbc0d38f6e6488623
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-05-16 14:14:13 +0530

    Add CRUD operations for bookings

--- backend/src/controllers/bookings.controller.js
+++ backend/src/controllers/bookings.controller.js
@@ -54,13 +54,102 @@ const createNewBooking = asyncHandler(async (req, res) => {
     .json(new ApiResponse(201, "Booking successful", newBooking));
 });
 
-const getBookingById = asyncHandler(async (req, res) => {});
+const getBookingById = asyncHandler(async (req, res) => {
+  const { bookingId } = req.params;
+  if (!bookingId) {
+    throw new ApiError(400, "bookingId is required");
+  }
+  if (!isValidObjectId(bookingId)) {
+    throw new ApiError(400, "Invalid bookingId");
+  }
+  const booking = await Booking.findById(bookingId);
+  if (!booking) {
+    throw new ApiError(404, "Booking not found");
+  }
+  return res.status(200).json(new ApiResponse(200, booking, "Booking found"));
+});
 
-const getAllBookings = asyncHandler(async (req, res) => {});
+const getAllBookings = asyncHandler(async (req, res) => {
+  const userId = req.user._id;
+  if (!userId) {
+    throw new ApiError(400, "userId is required");
+  }
+  if (!isValidObjectId(userId)) {
+    throw new ApiError(400, "Invalid userId");
+  }
+  const bookings = await Booking.find({ user: userId });
+  if (!bookings) {
+    throw new ApiError(404, "No bookings found");
+  }
+  return res.status(200).json(new ApiResponse(200, bookings, "Bookings found"));
+});
 
-const updateBookingById = asyncHandler(async (req, res) => {});
+const updateBookingById = asyncHandler(async (req, res) => {
+  const { bookingId } = req.params;
+  if (!bookingId) {
+    throw new ApiError(400, "bookingId is required");
+  }
+  if (!isValidObjectId(bookingId)) {
+    throw new ApiError(400, "Invalid bookingId");
+  }
+  const { checkinDate, checkoutDate, NumberOfRooms } = req.body;
+  if (!checkinDate || !checkoutDate || !NumberOfRooms) {
+    throw new ApiError(400, "checkIn, checkOut, NumberOfRooms are required");
+  }
+  const checkIn = new Date(checkinDate);
+  const checkOut = new Date(checkoutDate);
 
-const deleteBookingById = asyncHandler(async (req, res) => {});
+  if (isNaN(checkIn.getTime()) || isNaN(checkOut.getTime())) {
+    throw new ApiError(400, "Invalid date format");
+  }
+
+  if (checkIn >= checkOut) {
+    throw new ApiError(400, "Check-out date must be after check-in date");
+  }
+  const book = await Booking.findById(bookingId);
+  if (!book) {
+    throw new ApiError(404, "Booking not found");
+  }
+  const hotel = await Hotel.findById(book.hotel);
+  if (!hotel) {
+    throw new ApiError(404, "Hotel not found");
+  }
+  const price = hotel.price;
+  const numberOfNights = Math.ceil(
+    (checkOut - checkIn) / (1000 * 60 * 60 * 24)
+  );
+
+  const totalAmount = price * NumberOfRooms * numberOfNights;
+  const updateBooking = await Booking.findByIdAndUpdate(bookingId, {
+    $set: {
+      checkIn,
+      checkOut,
+      NumberOfRooms: NumberOfRooms,
+      totalAmount: totalAmount,
+    },
+  });
+  if (!updateBooking) {
+    throw new ApiError(500, "Booking update failed");
+  }
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updateBooking, "Booking updated"));
+});
+
+const deleteBookingById = asyncHandler(async (req, res) => {
+  const { bookingId } = req.params;
+  if (!bookingId) {
+    throw new ApiError(400, "bookingId is required");
+  }
+  if (!isValidObjectId(bookingId)) {
+    throw new ApiError(400, "Invalid bookingId");
+  }
+  const booking = await Booking.findByIdAndDelete(bookingId);
+  if (!booking) {
+    throw new ApiError(404, "Booking not found");
+  }
+  return res.status(200).json(new ApiResponse(200, {}, "Booking deleted"));
+});
 
 export {
   createNewBooking,
