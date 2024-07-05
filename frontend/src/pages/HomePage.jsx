3aaeb33cb3bf8ef1261e2bd0289121c1772bb04a
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-07-05 13:38:58 +0530

    chore: Add accordion component for FAQs on the homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -13,6 +13,12 @@ import config from "../config/config.js";
 import { useNavigate } from 'react-router-dom'
 import { Link } from 'react-router-dom'
 import { useDispatch,useSelector } from 'react-redux'
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
 
 function HomePage() {
     const [hotels, setHotels] = useState([]);
@@ -34,7 +40,9 @@ function HomePage() {
   }, [ ])
 
   return (
-    <div className='w-full min-h-96 flex flex-wrap items-center justify-center p-2.5'>
+    <div className='w-full min-h-96 flex flex-wrap items-center justify-center p-2.5 gap-10'>
+
+      {/* List of Hotels */}
     <div className='md:w-full w-[90%] min-h-96 flex flex-wrap items-center justify-evenly gap-5 mt-4 mb-4'>
       { hotels && hotels.map((hotel) => (
           console.log("hotel:",hotel._id),
@@ -59,6 +67,33 @@ function HomePage() {
         </Card>
       ))
       }
+
+    </div>
+
+
+    {/* FAQs */}
+    <div className='br-red-200 md:w-[50%] w-[90%] min-h-32'>
+      <h1 className="text-3xl font-bold text-center">FAQs</h1>
+      <Accordion type="single" collapsible className="w-full">
+      <AccordionItem value="item-1">
+        <AccordionTrigger>How do I make a reservation?</AccordionTrigger>
+        <AccordionContent>
+          To make a reservation, simply enter your destination, check-in and check-out dates, and the number of guests into the search bar. Browse the available hotels, select your preferred option, and follow the prompts to complete your booking.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-2">
+        <AccordionTrigger>Can I cancel or modify my booking?</AccordionTrigger>
+        <AccordionContent>
+         Yes, you can cancel or modify your booking depending on the hotel's policy. Please log in to your account, go to 'My Bookings', and follow the instructions to make changes. Note that cancellation and modification fees may apply.
+        </AccordionContent>
+      </AccordionItem>
+      <AccordionItem value="item-3">
+        <AccordionTrigger>What payment methods are accepted?</AccordionTrigger>
+        <AccordionContent>
+         We accept major credit cards (Visa, MasterCard, American Express), debit cards, and PayPal. All transactions are secure and encrypted for your protection.
+        </AccordionContent>
+      </AccordionItem>
+    </Accordion>
 
     </div>
       </div> 
