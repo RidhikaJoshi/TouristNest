d0f6ff67510d80793687caed675452f991b76f61
RidhikaJoshi <ridhikasmrh@gmail.com>
2024-07-05 16:39:08 +0530

    chore: Add pagination component for better navigation on the website

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -27,40 +27,73 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel"
 import Autoplay from "embla-carousel-autoplay"
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
+import { set } from 'date-fns';
 
 function HomePage() {
     const [hotels, setHotels] = useState([]);
-    const navigate = useNavigate();
+    const [page,setPage]=useState(1);
      const authStatus=useSelector((state)=>state.auth.status);
-
-     const Images=[
+     
+     const Images1=[
         {
           id:1,
-          image:"https://images.pexels.com/photos/2265876/pexels-photo-2265876.jpeg?auto=compress&cs=tinysrgb&w=600"
+          image:"https://images.pexels.com/photos/338504/pexels-photo-338504.jpeg?auto=compress&cs=tinysrgb&w=600"
         },
         {
           id:2,
-          image:"https://images.pexels.com/photos/731217/pexels-photo-731217.jpeg?auto=compress&cs=tinysrgb&w=600"
+          image:"https://images.pexels.com/photos/1458457/pexels-photo-1458457.jpeg?auto=compress&cs=tinysrgb&w=600"
         },
         {
           id:3,
-          image:"https://images.pexels.com/photos/1271619/pexels-photo-1271619.jpeg?auto=compress&cs=tinysrgb&w=600"
+          image:"https://images.pexels.com/photos/261102/pexels-photo-261102.jpeg?auto=compress&cs=tinysrgb&w=600"
         },
         {
           id:4,
-          image:"https://images.pexels.com/photos/586687/pexels-photo-586687.jpeg?auto=compress&cs=tinysrgb&w=600"
+          image:"https://images.pexels.com/photos/53464/sheraton-palace-hotel-lobby-architecture-san-francisco-53464.jpeg?auto=compress&cs=tinysrgb&w=600"
         },
         {
           id:5,
-          image:"https://images.pexels.com/photos/1010657/pexels-photo-1010657.jpeg?auto=compress&cs=tinysrgb&w=600"
+          image:"https://images.pexels.com/photos/70441/pexels-photo-70441.jpeg?auto=compress&cs=tinysrgb&w=600"
+        }
+     ]
+
+     const Images2=[
+        {
+          id:1,
+          image:"https://images.pexels.com/photos/2034335/pexels-photo-2034335.jpeg?auto=compress&cs=tinysrgb&w=600"
+        },
+        {
+          id:2,
+          image:"https://images.pexels.com/photos/895555/pexels-photo-895555.jpeg?auto=compress&cs=tinysrgb&w=600"
+        },
+        {
+          id:3,
+          image:"https://images.pexels.com/photos/261101/pexels-photo-261101.jpeg?auto=compress&cs=tinysrgb&w=600"
+        },
+        {
+          id:4,
+          image:"https://images.pexels.com/photos/1287460/pexels-photo-1287460.jpeg?auto=compress&cs=tinysrgb&w=600"
+        },
+        {
+          id:5,
+          image:"https://images.pexels.com/photos/261181/pexels-photo-261181.jpeg?auto=compress&cs=tinysrgb&w=600"
         }
      ]
 
   useEffect(() => {
     const fetchHotels = async () => {
       try {
-        const response = await axios.get(`${config.BASE_URL}/api/v1/hotels/getAllHotels`);
-        console.log("hotels:",response.data);
+        const response = await axios.get(`${config.BASE_URL}/api/v1/hotels/getAllHotels?page=${page}&limit=6`);
+        //console.log("hotels:",response.data);
         setHotels(response.data.data);
       } catch (error) {
         console.error('Error fetching hotels:', error)
@@ -68,12 +101,12 @@ function HomePage() {
     }
     fetchHotels();
 
-  }, [ ])
+  }, [page])
 
   return (
     <div className='w-full min-h-96 flex flex-wrap items-center justify-center p-2.5 gap-16'>
 
-      {/* Introduction */}
+      {/* Introduction1 */}
       <div className='md:w-full w-[90%] min-h-96  flex md:flex-row  flex-col flex-wrap items-center justify-center gap-4 mt-4'>
         <div className='md:w-[45%] w-full min-h-40 '>
             <h2 className='md:text-2xl text-xl font-semibold text-[#16A34A]'>"Discover your perfect stay with us! Book your dream vacation at unbeatable prices. Explore top-rated hotels and enjoy seamless booking for an unforgettable experience."</h2>
@@ -85,7 +118,7 @@ function HomePage() {
         }),
       ]}>
               <CarouselContent>
-                {Images.map((image,id) => (
+                {Images1.map((image,id) => (
                   <CarouselItem key={id}>
                     <div >
                       <Card>
@@ -105,13 +138,45 @@ function HomePage() {
       </div>
 
 
+    {/* Introduction2 */}
+       <div className='md:w-full w-[90%] min-h-96  flex md:flex-row  flex-col flex-wrap items-center justify-center gap-4 mt-4'>
+       
+        <div className='md:w-[45%] w-[80%] min-h-40  flex items-center justify-center'>
+            <Carousel className="w-full max-w-sm" opts={{align: "start",loop: true,}}  plugins={[
+        Autoplay({
+          delay: 2000,
+        }),
+      ]}>
+              <CarouselContent>
+                {Images2.map((image,id) => (
+                  <CarouselItem key={id}>
+                    <div >
+                      <Card>
+                        <CardContent className="flex aspect-square items-center justify-center p-6">
+                          <img src={image.image} alt="hotel" className="w-full h-full " />
+                        </CardContent>
+                      </Card>
+                    </div>
+                  </CarouselItem>
+                ))}
+              </CarouselContent>
+              <CarouselPrevious />
+              <CarouselNext />
+          </Carousel>
+        </div>
+         <div className='md:w-[45%] w-full min-h-40 '>
+            <h2 className='md:text-2xl text-xl font-semibold text-[#16A34A]'>"Stay in style, travel in comfort. Find the best deals on hotels, from budget-friendly to luxury. Make your next getaway unforgettable with our easy booking process."</h2>
+        </div>
+        
+      </div>
 
+              
       {/* List of Hotels */}
     <div className='md:w-full w-[90%] min-h-96 flex flex-wrap flex-col items-center justify-center gap-5 mt-4 mb-4'>
       {/* <h1 className="text-3xl font-bold text-center">Hotels</h1> */}
       <div className='w-full flex flex-row flex-wrap items-center justify-center gap-3'>
       { hotels && hotels.map((hotel) => (
-          console.log("hotel:",hotel._id),
+          //console.log("hotel:",hotel._id),
         <Card className="w-[400px] border-[0.5px] border-[#16A34A]" key={hotel._id}>
           <CardHeader>
             <CardTitle>{hotel.name}</CardTitle>
@@ -137,13 +202,47 @@ function HomePage() {
 
     </div>
 
+    <Pagination>
+      <PaginationContent>
+        <PaginationItem>
+          <PaginationPrevious onClick={() => {
+            setPage(page - 1>0?page-1:1);
+            
+          }} />
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink onClick={() => {
+            setPage(1);
+          }} >1</PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink onClick= {()=>{setPage(2);
+         }
+          } >
+            2
+          </PaginationLink>
+        </PaginationItem>
+        <PaginationItem>
+          <PaginationLink onClick= {()=>{setPage(3);
+          }
+          }>3</PaginationLink>
+        </PaginationItem>
+        {/* <PaginationItem>
+          <PaginationEllipsis />
+        </PaginationItem> */}
+        <PaginationItem>
+          <PaginationNext onClick= {()=>setPage(page+1<=3?page+1:3)} />
+        </PaginationItem>
+      </PaginationContent>
+    </Pagination>
+
 
     {/* FAQs */}
     <div className='br-red-200 md:w-[60%] w-[90%] min-h-32'>
       <h1 className="text-3xl font-bold text-center">FAQs</h1>
       <Accordion type="single" collapsible className="w-full">
       <AccordionItem value="item-1">
-        <AccordionTrigger>How do I make a reservation?</AccordionTrigger>
+        <AccordionTrigger className="text-lg">How do I make a reservation?</AccordionTrigger>
         <AccordionContent>
           To make a reservation, simply enter your destination, check-in and check-out dates, and the number of guests into the search bar. Browse the available hotels, select your preferred option, and follow the prompts to complete your booking.
         </AccordionContent>
